ee06b065780aca33a0370e87ca32b346c43ff3d5
Benedict Becker <becker.benedictj@gmail.com>
2016-03-22 19:39:13 -0400

    working back and forward button. Renamed the go button from push_button to goButton. I implemented back and forward using a vector and an int, but it would make more sense to use a list and an iterator. Maybe later

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    historyPlace=-1;
 }
 
 MainWindow::~MainWindow()
@@ -13,12 +15,39 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_goButton_clicked()
 {
     QString url = ui->lineEdit->text();
     std::string stdUrl = url.toStdString();
     std::size_t found = stdUrl.find("http://");
-    if (found == 0)
-        ui->webView->load(url);
-    else ui->webView->load("http://" + url);
+
+    if (found != 0){
+        url = "http://" + url;
+    }
+
+    ui->webView->load(url);
+    history.push_back(url.toStdString());
+    historyPlace++;
+}
+
+void MainWindow::on_backButton_clicked()
+{
+    if (historyPlace <= 0){
+        return;
+    }
+
+    historyPlace--;
+    ui->lineEdit->setText(QString::fromStdString(history[historyPlace]));
+    ui->webView->load(QString::fromStdString(history[historyPlace]));
+}
+
+void MainWindow::on_forwardButton_clicked()
+{
+    if (historyPlace >= history.size()-1){
+        return;
+    }
+
+    historyPlace++;
+    ui->lineEdit->setText(QString::fromStdString(history[historyPlace]));
+    ui->webView->load(QString::fromStdString(history[historyPlace]));
 }
